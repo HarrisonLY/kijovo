f8ea3e497719e23ff75ea2c602e94b6fffa7ef7d
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2014-10-16 09:58:35 -0400

    new form with cancel, delete, and add

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,45 @@
 class ProductsController < ApplicationController
+
 def index
   @products = Product.all
 end
+
 def show
   @product = Product.find(params[:id])
 end
+
+def edit
+  @product = Product.find(params[:id])
 end
+
+def update
+  @product = Product.find(params[:id])
+  @product.update(product_params)
+  redirect_to @product
+end
+
+def new
+  @product = Product.new
+end
+
+def create
+  @product = Product.new(product_params)
+  @product.save
+  redirect_to @product
+end
+
+def destroy
+  @product = Product.find(params[:id])
+  @product.destroy
+  redirect_to products_url
+end
+
+
+private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :releasing_on)
+  end
+
+  end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Kijovo::Application.routes.draw do
     root "products#index"
-    get "products" => "products#index"
-    get "products/:id" => "products#show", as: "product"
+    resources :products
 end
 
+
+    # get "products" => "products#index"
+    # get "products/:id" => "products#show", as: "product"
+    # get "products/:id/edit" => "products#edit", as: "edit_product"
+    # patch "products/:id" => "products#update"
