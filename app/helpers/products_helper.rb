66ed4887bfe51ffc8c9eb78485d8c7f443c11f3a
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2014-10-16 10:58:43 -0400

    order products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+#	def format_price(product)
+#	if product.TBD?
+#	   content_tag('TBD')
+#	else
+#		number_to_currency(product.price)
+#	end
+ # end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+
+	# def TBD?
+	#	price.blank? || price = TBD
+	# end
+
+	def self.upcoming
+		where("releasing_on >= ?", Time.now).order("releasing_on").order("name")
+	end
+
 end
