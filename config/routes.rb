814a33f97d819a22192ebdbb5c67d8459ce8cef4
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2014-10-17 16:15:46 -0400

    admin panel 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Kijovo::Application.routes.draw do
     root "welcome#index"
     resources :products
+    resources :admin 
 end
-
-
-    # get "products" => "products#index"
-    # get "products/:id" => "products#show", as: "product"
-    # get "products/:id/edit" => "products#edit", as: "edit_product"
-    # patch "products/:id" => "products#update"
